f975cdf7afe1cf20d26efc932e07e2053e713f51
nonanona115 <nonanona.115@gmail.com>
2025-06-12 00:37:24 +0900

    s13

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   output: 'standalone',
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
+  trailingSlash: false,      // ← これだけ追加
 };
