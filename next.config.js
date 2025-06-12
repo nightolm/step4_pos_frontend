740cac101b49c96ccc34d50ce06eb34b2d0dc32f
nonanona115 <nonanona.115@gmail.com>
2025-06-12 11:17:35 +0900

    s4^3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+// next.config.js
 module.exports = {
   reactStrictMode: true,
+  // 出力モードは standalone でも OK
   output: 'standalone',
-  // これを追加
-  trailingSlash: false,
-  staticPageGenerationTimeout: 300, // optional
-};
+  // クライアントに埋め込みたい変数
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+}
